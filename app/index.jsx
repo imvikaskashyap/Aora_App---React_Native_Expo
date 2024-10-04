917a4c399684d84561a1ad16a81997a865d45567
imvikaskashyap <mvikaskashyap@gmail.com>
2024-10-04 18:29:41 +0530

    folder structure done

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 const Index = () => {
   return (
     <View>
-      <Text style={{color: 'red', marginTop: 100, fontSize: 30}} >Index</Text>
-      <Link href="/profile" style={{color: 'blue', marginTop:10, textDecorationLine: 'underline'}}>Profile</Link>
+      <View className="flex items-center justify-center m-auto mt-40">
+      <Text className="text-3xl font-pbold text-red-600">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" className='text-blue-600 '>Profile</Link>
+    </View>
     </View>
   )
 }
 
 export default Index
 
-const styles = StyleSheet.create({
-  component:{
-    display: 'flex',
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
--- app/profile.jsx
+++ app/profile.jsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const Profile = () => {
   return (
     <View>
       <Text style={{color: 'red', marginTop: 100, fontSize: 30}}>Profile</Text>
+      <Link href="/" className='text-blue-600'>Home</Link>
     </View>
   )
 }
