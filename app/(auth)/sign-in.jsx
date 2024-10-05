3f0667c89a7b9d05c8c83f40802c174d0d821926
imvikaskashyap <mvikaskashyap@gmail.com>
2024-10-05 12:24:08 +0530

    signup complete

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
-import CustomButton from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
+import { Link } from 'expo-router'
 
 const SignIn = () => {
 
@@ -12,10 +13,14 @@ const SignIn = () => {
   password:""
   })
 
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = ()=>{}
+
   return (
   <SafeAreaView className="bg-primary h-full">
     <ScrollView>
-      <View className={`w-full justify-center h-full px-4 my-6`}>
+      <View className={`w-full justify-center min-h-[83vh] px-4 my-6`}>
         <Image source={images.logo} resizeMethod='contain' className="w-[115px] h-[35px]"/>
         <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora!</Text>
 
@@ -34,7 +39,17 @@ const SignIn = () => {
           keyboardType="password"
         />
 
-        <CustomButton title="Sign In"/>
+        <CustomButton title="Sign In"
+          handlePress={submit}
+          containerStyles="mt-7"
+          isLoading={isSubmitting}
+        />
+
+        <View className="justify-center pt-5 flex-row gap-2">
+          <Text className="text-lg text-gray-100 font-psemibold">Don't have an account?</Text>
+          <Link href="/sign-up" className='text-lg font-psemibold text-secondary'>Sign Up</Link>
+        </View>
+
 
       </View>
      
@@ -44,3 +59,6 @@ const SignIn = () => {
 }
 
 export default SignIn
+
+
+
